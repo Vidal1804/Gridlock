9b73e7dc949608b3703fab8e1214bca7ada282e3
streate <69007916+Vidal1804@users.noreply.github.com>
2026-05-21 18:58:21 +0300

    Update StartController.php

--- controllers/StartController.php
+++ controllers/StartController.php
@@ -1,9 +1,16 @@
 <?php
-if(session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
 
 if($_SERVER['REQUEST_METHOD'] === 'GET'){
+
+    if(session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    if(isset($_SESSION['user_id'])){
+        header("Location: /home");
+        exit();
+    }
+
     require_once __DIR__ . '/../views/StartView.php';
     exit();
 }
