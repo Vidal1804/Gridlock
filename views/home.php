6da9183ac5e09bcb77953f41f7f688839c8130be
streate <69007916+Vidal1804@users.noreply.github.com>
2026-05-21 16:18:53 +0300

    Update home.php

--- views/home.php
+++ views/home.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 <body>
-    <?php
-        if(isset($_COOKIE["name"])){
-            echo "Hello, " . htmlspecialchars($_COOKIE["name"]);
-        }
-        else {
-            header("Location: ../views/AccountsView.php");
-            exit();
-        }
-    ?>
+    <h1>Welcome to the Home Page!</h1>
 
     <form method="get" action="../controllers/logout.php">
         <input type="submit" value="Log out";>
