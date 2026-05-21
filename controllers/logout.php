a0e910d2a2c646a113fd765271190fcb51ec388e
streate <69007916+Vidal1804@users.noreply.github.com>
2026-05-21 16:54:38 +0300

    Update logout.php

--- controllers/logout.php
+++ controllers/logout.php
@@ -1,7 +1,9 @@
 <?php
     
+    session_start();
+
     if(isset($_SESSION['user_id'])){
-        setcookie(session_name(), '', time()-42000);
+        setcookie(session_name(), '', time()-42000, '/');
         $_SESSION = array();
         session_destroy();
     }
