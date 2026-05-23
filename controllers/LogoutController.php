c9a170a749b5ca3b83110fed9822ff11918c1a09
streate <69007916+Vidal1804@users.noreply.github.com>
2026-05-24 00:04:25 +0300

    changed logout to a post request

--- controllers/LogoutController.php
+++ controllers/LogoutController.php
@@ -1,13 +1,16 @@
 <?php
     
-    session_start();
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        session_start();
 
-    if(isset($_SESSION['user_id'])){
-        setcookie(session_name(), '', time()-42000, '/');
-        $_SESSION = array();
-        session_destroy();
+        if(isset($_SESSION['user_id'])){
+            setcookie(session_name(), '', time()-42000, '/');
+            $_SESSION = array();
+            session_destroy();
+        }
+        
+        header("Location: /login");
+        exit();
     }
-
-    header("Location: /login");
-    exit();
+    
 ?>
--- views/home.php
+++ views/home.php
@@ -16,16 +16,13 @@ $username = isset($_SESSION['username']) ? $_SESSION['username'] : 'Error';
 </head>
 <body>
 
+<!-- Am schimbat aici in POST request ca sa fie secure-->
     <header class="top-nav">
-    <form method="get" action="/logout" style="margin: 0;">
+    <form method="post" action="/logout" style="margin: 0;">
         <input type="submit" value="Log out" class="nav-btn primary-btn">
     </form>
     </header>
 
-    <!--  Ca idee, am vazut ca merge si metoda asta daca vrei, si e mai scurta, dar cu form e mai clar banuiesc. Da am vzt ca astea forms baga si un outline la buton, nush
-     <header class="top-nav">
-        <a href="/logout" class="nav-btn primary-btn">Log out</a>
-     </header> -->
 
     <main class="info-section">
         <h1 class="info-title">Welcome, <?php echo htmlspecialchars($username, ENT_QUOTES, 'UTF-8');?>!</h1>
