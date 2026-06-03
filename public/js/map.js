3a071461a713c15087c7a03c648a5037897741b9
streate <69007916+Vidal1804@users.noreply.github.com>
2026-06-04 00:02:15 +0300

    changed map from stadia to cartodb for dark mode map

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,8 @@
 const map = L.map('map').setView([40.775, -73.972], 15);
 
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="https://openstreetmap.org">OpenStreetMap</a>',
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
     maxZoom: 20
 }).addTo(map);
 
