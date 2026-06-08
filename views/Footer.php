8761048e91e8fd0829544ebe2533a7aca0430816
alexinio05 <constantinbarbieru@yahoo.com>
2026-06-08 23:15:08 +0300

    footer fix

--- views/Footer.php
+++ views/Footer.php
@@ -1,4 +1,4 @@
 <footer class="footer-style">
-    <p style="margin: 0;">Gridlock &copy; <?php echo date('Y'); ?></p>
-    <a href="/attributes" style="text-decoration: none;"><p style="margin: 0; color: white;">Resources</p></a>
+    <p>Gridlock &copy; <?php echo date('Y'); ?></p>
+    <a href="/attributes" class="footer-link">Resources</a>
 </footer>
